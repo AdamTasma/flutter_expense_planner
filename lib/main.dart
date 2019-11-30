0314e1d23f14c5f90678af84305b91afcac92c0b
AdamTasma <adamtasma@gmail.com>
2019-11-29 22:10:44 -0500

    get ready to map transactions to cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
-void main() => runApp(MyHomePage());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App Expense Planner',
+      home: MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'shoes',
+      amount: 59.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'coat',
+      amount: 135.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,12 +36,23 @@ class MyHomePage extends StatelessWidget {
         title: Text('Expense Planner'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Card(
-            child: Text('chart'),
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('chart'),
+              elevation: 5,
+            ),
           ),
-          Card(
-            child: Text('list of transactions'),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
           ),
         ],
       ),
