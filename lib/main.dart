7ad8b47e7f3c0294743f3a1e0830d2006fc02cea
AdamTasma <adamtasma@gmail.com>
2020-08-01 16:49:08 -0400

    add "new_transaction" and "user_transactions" widgets, refactor the code from main into these new widgets respectively

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,21 +13,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'shoes',
-      amount: 59.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'coat',
-      amount: 135.99,
-      date: DateTime.now(),
-    ),
-  ];
-
 //  String titleInput;
 //  String amountInput;
   final titleController = TextEditingController();
@@ -53,79 +36,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(labelText: "Title"),
-                    // onChanged: (myVal) => titleInput = myVal, shorter notation example
-//                    onChanged: (myVal) {
-////                      titleInput = myVal;
-////                    },
-                    controller: titleController,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: "Amount"),
-                    controller: amountController,
-//                    onChanged: (val) => amountInput = val,
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                    onPressed: () {
-                      // todo add the input to the list of transactions
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transactions.map((transaction) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${transaction.amount}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                      Text(
-                        transaction.title,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(DateFormat.yMMMd().format(transaction.date),
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          )),
-                    ])
-                  ],
-                ),
-              );
-            }).toList(),
-          ),
+
         ],
       ),
     );
